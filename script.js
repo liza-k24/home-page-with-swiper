23f909225503cbd0c0403ce787424e7ac5d73b51
Liza <lizaveta.fox83@gmail.com>
2025-08-05 22:32:02 +0300

    swiper

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ const linkAction = () =>{
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const swiperHome = new Swiper('.home_swiper', {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: (index, className) => {
+          return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + "</span>";
+        },
+    },
+
+    // autoplay: {
+    //     delay: 5000,
+    // }
+    
+})
+
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+
+window.addEventListener('scroll', bgHeader)
